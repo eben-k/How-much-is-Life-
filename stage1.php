d938cd14147ff67e822e5557c231381713478686
Eben Ennim <ennim.consults@gmail.com>
2018-04-30 14:11:07 +0000

    first version of stage1

---
+++ stage1.php
@@ -0,0 +1,48 @@
+<?php
+echo "<!DOCTYPE html>
+<html>
+
+<head>
+	 <link rel='stylesheet' href='style.css'/>
+
+
+  <h1>How Much Is Life..?</h1>
+</head>
+
+<body>
+<style>body{
+    position: relative;
+    width: 1400px;
+    height: 1000px;
+    background-color: #67B2CA
+}
+h1{
+    position: absolute;
+    width: 800px;
+    height: 180px;
+    left: 300px;
+    top: 280px;
+    font-family: Poor Family;
+    font-style: italic;
+    font-weight: normal;
+    font-size: 90px;
+    line-height: normal;
+    color: #077D39;
+}
+</style>
+</body>
+</html>"
+
+?>
+<?php
+echo 
+ "Current time:" .date('h:i:sa');
+?>
+
+
+
+
+
+
+
+
